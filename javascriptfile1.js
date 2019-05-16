2aa8d754ac92a5cb98e4ce4dc410d2457d5c880b
Sukhbinder Kaur Barring <735833@student.tor.loc>
2019-05-16 17:49:52 -0400

    first commit

---
+++ javascriptfile1.js
@@ -0,0 +1,6 @@
+let item1price=20;
+let item2price=30;
+let sum = item1price + item2price;
+let firstname = "joe";
+let lastname = "Smith";
+alert("HELLO" + firstname+ "" +lastname+ "your total is"+ sum);
